7a4f19d0181f9fc0ee452d6a7f2cd8330f4f3fd7
zhouyu <zhouyu@firstbrave.com>
2020-01-20 21:53:13 +0800

    breeze.

---
+++ src/main/scala/com/zy/yuanli/BreezeplotSampleOne.scala
@@ -0,0 +1,14 @@
+package com.zy.yuanli
+
+import breeze.linalg._
+import breeze.plot._
+/**
+  * #create 2020-01-20
+  * @author zhouyu
+  * @desc keshihau
+  */
+object BreezeplotSampleOne {
+  def main(args: Array[String]): Unit = {
+      val f = new Figure()
+  }
+}
